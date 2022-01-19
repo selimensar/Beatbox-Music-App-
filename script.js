7d0b248a1d75d53c926959ddf21080b56b60da7f
Selim <selim@192-168-2-106-Fikri.fritz.box>
2022-01-19 12:05:45 +0100

    the app is completed

--- script.js
+++ script.js
@@ -5,33 +5,71 @@
 // TODO: Set the colors programatically
 
 window.onload = () => {
-    setColors();
+    setButtons();
 }
  
 let beats = {
-    "a":  "./assets/pianor-chord-331.mp3",
-
-    "s": "./assets/piano-chord-209.mp3",
-    "d": "./assets/piano-chord-208.mp3",
-    "f": "./assets/drum-sticks-hit.mp3",
-    "g": "./assets/drum-snare-roll.mp3",
-    "h": "./assets/prel-musical.mp3",
-    "j": "./assets/cybal-suspended.mp3",
-    "k": "./assets/musical-compos.mp3",
-    "l": "./assets/musical-orches.mp3"
+    "a": {
+        beat: "./assets/pianor-chord-331.mp3",
+        color: "#00fffe"
+    },
+    "s": {
+        beat: "./assets/piano-chord-209.mp3",
+        color: "#00fffe"
+    },
+    "d": {
+        beat: "./assets/piano-chord-208.mp3",
+        color: "#00fffe"
+    },
+    "f": {
+        beat:  "./assets/drum-sticks-hit.mp3",
+        color: "#ff00ff"
+    },
+    "g": {
+        beat: "./assets/drum-snare-roll.mp3",
+        color: "#FF00FF"
+    },
+    "h": {
+        beat: "./assets/prel-musical.mp3",
+        color: "#ff00ff"
+    },
+    "j": {
+        beat: "./assets/cybal-suspended.mp3",
+        color: "#FF00FF"
+    },
+    "k": {
+        beat: "./assets/musical-compos.mp3",
+        color: "#FFFFFF"
+    },
+    "l": {
+        beat: "./assets/musical-orches.mp3",
+        color: "#FFFFFF"
+    }
     
 }
 
 // #FF00FF
-const setColors = () => {
-    const element = document.getElementById('f');
-    element.style.borderColor = "#FF00FF";
+const setButtons = () => {
+    for(let beatKey in beats) {
+    let beat = beats[beatKey];
+    const element = document.getElementById(beatKey);
+    element.style.borderColor = beat.color;
+    element.addEventListener("transitionend", ()=>{
+        element.style.backgroundColor = "transparent";
+        element.style.boxShadow = "none";
+    })
+    }   
+}
 
+const onButtonPress = (buttonKey) => {
+    let element = document.getElementById(buttonKey);
+    element.style.backgroundColor = beats[buttonKey].color;
+    element.style.boxShadow = `0px 0px 17px 0px ${beats[buttonKey].color}`;
 }
 
 
 const playBeat = (buttonKey) => {
-        let audioSrc = beats[buttonKey];
+        let audioSrc = beats[buttonKey].beat;
         let audio = new Audio(audioSrc);
         audio.currentTime = 0;
         audio.play();
@@ -42,5 +80,6 @@ const playBeat = (buttonKey) => {
 document.addEventListener('keydown', (event)=>
 {
     playBeat(event.key);
+    onButtonPress(event.key);
 })
 
